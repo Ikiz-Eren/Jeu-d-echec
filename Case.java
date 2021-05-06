0a5fc3a2014700dfc3f4f2744f494d2f64136080
Ikiz-Eren <81411116+Ikiz-Eren@users.noreply.github.com>
2021-05-07 01:12:08 +0200

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,19 @@
+public class Case{
+	private int ligne;
+	private int colonne;
+	private Piece Piece
+	private Couleur couleur;
+
+	public getLigne(){
+		return ligne;
+	}
+	public getColonne(){
+		return colonne;
+	}
+	public getPiece(){
+		return Piece;
+	}
+	public getCouleur(){
+		return Couleur;
+	}
+}
---
+++ Coup.java
@@ -0,0 +1,13 @@
+public class Coup{
+	private case CaseDepart;
+	private case CaseArrive;
+
+	public case getCaseDepart(){
+		return CaseDepart;
+	}
+
+	public case getCaseArrive(){
+		return CaseArrive;
+	}
+
+}
